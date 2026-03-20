e72bb4ac6fe3c4f25655dd7bf39c6666c1a30747
Stephen Meisenbacher <sjmeis@gtgd.com>
2026-03-20 11:43:10 +0100

    handle threading conflict

--- src/TaxonomyBuilder/core.py
+++ src/TaxonomyBuilder/core.py
@@ -1,3 +1,8 @@
+import os
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+
 import logging
 from tqdm.auto import tqdm
 import torch
